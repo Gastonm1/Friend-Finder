469790d28d8245dd693a699caf9a5b990464f875
Gastonm1 <gaston.marques@gmail.com>
2019-03-07 18:43:17 -0600

    apiRoutes

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,3 +1,24 @@
 // -------------------------------------------------------------------------------
-// LISTENER
+// Load Data
 // -------------------------------------------------------------------------------
+
+var friends = require("../app/public/friends.js");
+
+// -------------------------------------------------------------------------------
+// Routing
+// -------------------------------------------------------------------------------
+
+module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res) {
+        res.json(friendData);
+      });
+
+      app.post("/api/friends", function(req, res) {
+
+        if (tableData.length < 5) {
+          friendData.push(req.body);
+          res.json(true);
+        }
+      });
+    }

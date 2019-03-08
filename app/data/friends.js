9c41afb73aa27fb46b8d382063dec17e7ca39fde
Gastonm1 <gaston.marques@gmail.com>
2019-03-07 21:24:08 -0600

    questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,33 @@
 var friendArray = [
     {
-      friendName: "Friend",
-      friendEmail: "friend@friend.com",
-      phoneNumber: "000-000-0000"
+      friendName: "Dwayne Johnson",
+      friendPhoto: "https://i.pinimg.com/originals/54/61/d2/5461d21528d3db2d59b22a875510f22c.jpg",
+      phoneNumber: "000-000-0001",
+      scores: ["2", "3", "4","1","5","2","3","2","5","3"]
+    },
+    {
+      friendName: "Hagrid",
+      friendPhoto: "https://s.aolcdn.com/dims-shared/dims3/GLOB/crop/580x380+0+0/resize/1028x675!/format/jpg/quality/85/https://s.aolcdn.com/hss/storage/midas/d7cd49ec0d417b80fb2034638ed37fb7/203283742/Screen+Shot+2016-01-19+at+4.18.36+PM.png",
+      phoneNumber: "000-000-0002",
+      scores: ["5", "3", "1","2","3","1","4","2","5","3"]
+    },
+    {
+      friendName: "Dave Chapelle",
+      friendPhoto: "https://static01.nyt.com/images/2017/03/18/arts/18CHAPPELLE-INTERVIEW/18CHAPPELLE-INTERVIEW-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+      phoneNumber: "000-000-0003",
+      scores: ["4", "5", "2","4","3","5","1","2","3","2"]
+    },
+    {
+      friendName: "Coach Ditka",
+      friendPhoto: "https://i.kinja-img.com/gawker-media/image/upload/s--wlPIybE7--/18s4dt70alszpjpg.jpg",
+      phoneNumber: "000-000-0004",
+      scores: ["1", "2", "3","4","1","2","3","4","1","2"],
+    },
+    {
+      friendName: "Bruno Mars",
+      friendPhoto: "https://cdn.newsapi.com.au/image/v1/cf03ccf2346783fd19f03ea94ef7e1bb",
+      phoneNumber: "000-000-0005",
+      scores: ["5", "3", "2","5","4","2","4","1","1","3"]
     }
   ];
 

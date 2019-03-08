53a49baf72f7a23d240fc234e03e2aa596eb5495
Gastonm1 <gaston.marques@gmail.com>
2019-03-07 18:19:42 -0600

    update

--- server.js
+++ server.js
@@ -11,9 +11,16 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+//-------------------------------------------------------------------------------
+// ROUTER
 require("./app/public/home.html")(app);
 require("./app/public/survey.html")(app);
+//-------------------------------------------------------------------------------
 
+// -------------------------------------------------------------------------------
+// LISTENER
+// -------------------------------------------------------------------------------
 
-// Users (DATA)
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});

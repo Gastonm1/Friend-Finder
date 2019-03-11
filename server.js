61657e883f664b856b1e57f080a10e5691bb761b
Gastonm1 <gaston.marques@gmail.com>
2019-03-10 19:15:12 -0500

    update

--- server.js
+++ server.js
@@ -25,5 +25,6 @@ require("./routing/htmlRoutes")(app);
 // -------------------------------------------------------------------------------
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log("App listening on http://localhost:" + PORT);
+
 });

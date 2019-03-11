0199b50d52954a4d1f8f814375fece7f6fbd7fc0
Gastonm1 <gaston.marques@gmail.com>
2019-03-10 20:00:05 -0500

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -8,26 +8,26 @@ var friendArray = [
       photo: "https://i.pinimg.com/originals/54/61/d2/5461d21528d3db2d59b22a875510f22c.jpg",
       scores: ["2", "3", "4","1","5","2","3","2","5","3"]
     },
-    {
-      name: "Hagrid",
-      photo: "https://s.aolcdn.com/dims-shared/dims3/GLOB/crop/580x380+0+0/resize/1028x675!/format/jpg/quality/85/https://s.aolcdn.com/hss/storage/midas/d7cd49ec0d417b80fb2034638ed37fb7/203283742/Screen+Shot+2016-01-19+at+4.18.36+PM.png",
-      scores: ["5", "3", "1","2","3","1","4","2","5","3"]
-    },
-    {
-      name: "Dave Chapelle",
-      photo: "https://static01.nyt.com/images/2017/03/18/arts/18CHAPPELLE-INTERVIEW/18CHAPPELLE-INTERVIEW-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
-      scores: ["4", "5", "2","4","3","5","1","2","3","2"]
-    },
-    {
-      name: "Coach Ditka",
-      photo: "https://i.kinja-img.com/gawker-media/image/upload/s--wlPIybE7--/18s4dt70alszpjpg.jpg",
-      scores: ["1", "2", "3","4","1","2","3","4","1","2"]
-    },
-    {
-      name: "Bruno Mars",
-      photo: "https://cdn.newsapi.com.au/image/v1/cf03ccf2346783fd19f03ea94ef7e1bb",
-      scores: ["5", "3", "2","5","4","2","4","1","1","3"]
-    }
+    // {
+    //   name: "Hagrid",
+    //   photo: "https://s.aolcdn.com/dims-shared/dims3/GLOB/crop/580x380+0+0/resize/1028x675!/format/jpg/quality/85/https://s.aolcdn.com/hss/storage/midas/d7cd49ec0d417b80fb2034638ed37fb7/203283742/Screen+Shot+2016-01-19+at+4.18.36+PM.png",
+    //   scores: ["5", "3", "1","2","3","1","4","2","5","3"]
+    // },
+    // {
+    //   name: "Dave Chapelle",
+    //   photo: "https://static01.nyt.com/images/2017/03/18/arts/18CHAPPELLE-INTERVIEW/18CHAPPELLE-INTERVIEW-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+    //   scores: ["4", "5", "2","4","3","5","1","2","3","2"]
+    // },
+    // {
+    //   name: "Coach Ditka",
+    //   photo: "https://i.kinja-img.com/gawker-media/image/upload/s--wlPIybE7--/18s4dt70alszpjpg.jpg",
+    //   scores: ["1", "2", "3","4","1","2","3","4","1","2"]
+    // },
+    // {
+    //   name: "Bruno Mars",
+    //   photo: "https://cdn.newsapi.com.au/image/v1/cf03ccf2346783fd19f03ea94ef7e1bb",
+    //   scores: ["5", "3", "2","5","4","2","4","1","1","3"]
+    // }
   ];
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -9,21 +9,31 @@ var friendData = require("../app/data/friends.js");
 // -------------------------------------------------------------------------------
 
 module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
 
-    app.get("/api/friends", function(req, res) {
-        res.json(friendData);
-      });
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body);
+    var userScores = req.body.scores;
+    var friendsTotal = [];
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    for (var i = 0; i < friendData.length; i++) {
+      var currentFriend = friendData[i];
+      currentFriend.scores;
 
-      app.post("/api/friends", function(req, res) {
-        console.log(req.body)
-        var userScores = req.body.scores;
-        
-        var matchFound = {
-          name: "",
-          photo: ""
-        };
-          // res.json(true);
-          res.send("hello world")
-        }
-      )};
-    
+      for (var j = 0; j < currentFriend.scores.length; j++) {
+        var friendScore = currentFriend.scores[j];
+        var userScore = userScores[j];
+        console.log(Math.abs(userScore - friendScore)); //<--- Math.abs produces all absolute values
+      }
+    }
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    var matchFound = {
+      name: "",
+      photo: ""
+    };
+    // res.json(true);
+    res.send("hello world");
+  });
+};
